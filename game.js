31fb3ace05ecafaca55e0abbaa7704bbc924bd8b
Garland <garland220@yahoo.com>
2016-06-19 11:22:55 -0400

    Added project settings

--- game.js
+++ game.js
@@ -1,9 +1,10 @@
 
-/*global engine*/
 (function() {
   'use strict';
 
+  /*global engine*/
   engine.canvas = document.getElementById('canvas');
   engine.context = engine.canvas.getContext('2d');
   engine.update();
+
 })();
