4a1f8f90db10edf9f4482cee876da3e49fd3b80a
GitGHD <Gdexyan@yandex.com>
2016-06-19 21:24:31 -0400

    Setting up background animations

--- js/map.js
+++ js/map.js
@@ -10,7 +10,31 @@
     this.height = 50;
     this.width = 50;
 
+    this.current = 1;
+
+    this.backgrounds = {
+      0: {speed: 0, pattern: 0},
+      1: {speed: 5, pattern: 1}
+    };
+
+    this.test = 600;
+
     this.draw = function(context) {
+      var speed = this.backgrounds[this.current].speed;
+
+      context.fillStyle = 'black';
+      context.fillRect(0, 0, 900, 600);
+
+      if (this.current > 0) {
+
+        context.fillStyle = 'white';
+        this.test += speed;
+        if (this.test > 1200) {
+          this.test = 0;
+        }
+        context.fillRect(0, this.test-600, 900, 600);
+
+      }
 
     };
   };
