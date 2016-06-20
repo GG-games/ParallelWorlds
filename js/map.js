a7b465ae74026dc61a872471d07b481038d6ab72
GitGHD <Gdexyan@yandex.com>
2016-06-19 23:54:34 -0400

    More background animations

--- js/map.js
+++ js/map.js
@@ -10,11 +10,12 @@
     this.height = 50;
     this.width = 50;
 
-    this.current = 1;
+    this.current = 2;
 
     this.backgrounds = {
       0: {speed: 0, pattern: 0},
-      1: {speed: 5, pattern: 1}
+      1: {speed: 5, pattern: 1},
+      2: {speed: 5, pattern: 2}
     };
 
     this.bgPosition = 0;
@@ -27,7 +28,7 @@
 
       context.clearRect(0, 0, width, height);
 
-      if (this.current > 0) {
+      if (this.current === 1) {
 
         context.fillStyle = 'white';
 
@@ -45,6 +46,29 @@
         else {
           this.bgPosition += speed;
           context.fillRect(0, height - this.bgPosition, width, height);
+        }
+
+      }
+      else if (this.current === 2) {
+
+        context.fillStyle = 'white';
+
+        if (this.bgPosition > width) {
+          this.reverse = true;
+        }
+        if (this.bgPosition <= 0) {
+          this.reverse = false;
+        }
+
+        if (this.reverse) {
+          this.bgPosition -= speed;
+          context.fillRect(width - this.bgPosition, 0, width, height/2);
+          context.fillRect(0, height/2, this.bgPosition, height/2);
+        }
+        else {
+          this.bgPosition += speed;
+          context.fillRect(0, 0, this.bgPosition, height/2);
+          context.fillRect(width - this.bgPosition, height/2, width, height/2);
         }
 
       }
